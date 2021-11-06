b44a78a0d052c1035d92c875bf91d279fb608162
heltonandreazza <helton.prg@gmail.com>
2021-11-06 12:12:16 -0300

    wip

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,30 +4,7 @@ import '../styles/global.css'
 
 const Index = () => (
   <main>
-    <section className="h-screen">
-      <Hero />
-      {/* <section className="h-2/3">
-        <section className="flex flex-col md:flex-row h-1/6"></section>
-        <section className="flex flex-col md:flex-row h-5/6">
-          <div className="flex flex-col flex-1 justify-center p-8">
-            <h1 className="text-6xl font-bold py-8">
-              Data to enrich your{' '}
-              <span className="text-blue-500">online business</span>
-            </h1>
-            <p className="font-medium text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-            </p>
-          </div>
-          <div className="flex-1 bg-red-100">
-            <img
-              className="w-full h-full object-cover"
-              src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2102&amp;q=80" alt="">
-            </img>
-          </div>
-        </section>
-      </section>
-      <section className="h-1/3">clientes</section> */}
-    </section>
+    <Hero />
 
     {/* section brands */}
     {/* section services */}
