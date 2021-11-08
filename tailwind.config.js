f95e6d64b8ccb529646a5a7c886b4c060b8f8525
JeanDaRosa <unrealsaifer@gmail.com>
2021-11-08 20:58:38 -0300

    create componet meetUs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        meetUs:
+          "url('http://finance.bold-themes.com/main-demo/wp-content/uploads/sites/4/2016/07/background_4.jpg')",
+      },
+    },
   },
   variants: {
     extend: {},
